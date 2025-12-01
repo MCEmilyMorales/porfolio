d0a72d2d6c5b1d13c9b81b118a1e2bfddce1ecbd
Emily Morales <15ruth05@gmail.com>
2025-12-01 17:11:45 -0300

    feat:new link para redireccionar a un sitio web

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -1,6 +1,7 @@
 import BoxProyecto from "@/components/ui/BoxProyecto";
 import Titulos from "@/components/ui/Titulos";
 import Image from "next/image";
+import Link from "next/link";
 
 const Proyectos = () => {
   return (
@@ -8,17 +9,57 @@ const Proyectos = () => {
       <Titulos text="Mis proyectos" />
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-20 py-16 px-4 md:px-24">
         <BoxProyecto>
-          <Image
-            src={"/icons/postgresql.svg"}
-            alt="e commerce"
-            width={90}
-            height={90}
-            className="border-2  w-full"
-          />
-          <p>
-            Proyecto es una tienda de productos. <br />
-            En Proceso...
-          </p>
+          <Link
+            href={"https://eccomerce-flame.vercel.app/"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="font-bold text-2xl hover:text-amber-500 py-3 ">
+              {" "}
+              Eccomerce
+            </p>
+          </Link>
+
+          <div className="relative w-full h-80 overflow-hidden">
+            <Image
+              src={"/image/registro.png"}
+              alt="e commerce"
+              fill
+              priority
+              sizes="100vw"
+            />
+            <Link
+              href={"https://eccomerce-flame.vercel.app/"}
+              target="_blank"
+              rel="noopener noreferrer"
+            ></Link>
+          </div>
+          <div className="pt-4 space-y-2">
+            <Link
+              href={"https://eccomerce-flame.vercel.app/"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="text-xl font-semibold">Tienda de productos</p>
+            </Link>
+
+            <hr className="border text-yellow-300" />
+            <p>
+              {" "}
+              <strong>Front</strong> con Next.js, Tailwind, Axios. Deployado
+              VERCEL
+            </p>
+
+            <p>
+              {" "}
+              <strong>Back </strong> con Nest.js, JWT, Cookies,TypeOrm,
+              Cloudinary, Swagger
+            </p>
+
+            <p>
+              <strong>Base de datos </strong> con PostgreSQL Deployado Render
+            </p>
+          </div>
         </BoxProyecto>
       </div>
     </>
